50fce07954092f403be3039d52eaa7a9e6cb1b43
Ba136913 <121050352+Ba136913@users.noreply.github.com>
2026-03-26 08:51:39 +0000

    env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
-
+import tailwindcss from '@tailwindcss/vite'
 import viteReact from '@vitejs/plugin-react'
 import path from 'path'
 
